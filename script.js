47d3132d23f581738fc1af7403da5d3b10aaad31
aminulkarim26 <aminul.mkt@gmail.com>
2020-08-05 21:18:06 +0600

    pin matcher

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
-var displayPin= document.getElementById('display');
-    
-const generateBtn = document.getElementById("generate");
-     generateBtn.addEventListener("click", function(){
-         var arr =[];
-         for(var i=0; i<4 ; i++){
-         var randomNum = Math.random () *9;
-         var generatePin = Math.round(randomNum);
-         arr.push(generatePin);
+var displayPin = document.getElementById('display');
 
-         
-         displayPin.value = arr.join("");
-        
-    
-        }    
+const generateBtn = document.getElementById("generate");
+generateBtn.addEventListener("click", function () {
+    var arr = [];
+    for (var i = 0; i < 4; i++) {
+        var randomNum = Math.random() * 9;
+        var generatePin = Math.round(randomNum);
+        arr.push(generatePin);
+
+
+        displayPin.value = arr.join("");
+
+
+    }
 })
 
-function buttonNumber(id){
+function buttonNumber(id) {
     return document.getElementById(id);
 }
 const btn = document.getElementsByClassName("button");
-    for(i = 0; i< btn.length; i++){
-    btn[i].addEventListener("click", function(){
+for (i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function () {
         let numberBtn = this.innerHTML;
-        if(numberBtn == "C"){
+        if (numberBtn == "C") {
             buttonNumber("numbersArea").value = "";
         }
-        else if(numberBtn == "<") {
+        else if (numberBtn == "<") {
             let numberBtn = buttonNumber("numbersArea").value;
             let removeNumber = numberBtn.slice(0, -1);
             buttonNumber("numbersArea").value = removeNumber;
 
         }
-        else{
+        else {
             buttonNumber("numbersArea").value += numberBtn;
         }
     })
 
 }
 
+var submitPin = document.getElementById("submitted");
+var numberBtn = document.getElementById("numbersArea").value;
+var generatedPin = document.getElementById("display").value;
+
+submitPin.addEventListener("click", function () {
+
+    if (numberBtn === generatedPin) {
+        document.getElementById("matched").style.display = "block";
+
+
+    } else {
+        document.getElementById("not-matched").style.display = "block";
+    }
+
+
+}
+)
